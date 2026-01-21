e21ce09727a5b513c3db251f2616d2ece925e432
Yash Chetan Doshi <132264642+candelatesla@users.noreply.github.com>
2026-01-21 15:08:54 -0600

    Day2

---
+++ SQL50/Basic Aggregate Functions/1633. Percentage of Users Attended a Contest.sql
@@ -0,0 +1,7 @@
+
+SELECT r.contest_id, ROUND(COUNT(r.contest_id)/(SELECT COUNT(user_id) FROM Users)*100,2) AS percentage
+FROM Users u
+JOIN Register r
+ON r.user_id = u.user_id
+GROUP BY  r.contest_id
+ORDER BY percentage DESC, r.contest_id ASC
