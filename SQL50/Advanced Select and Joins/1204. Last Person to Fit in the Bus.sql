baf38290ab7f424eb1e16a3b78dea467b32e1b22
Yash Chetan Doshi <132264642+candelatesla@users.noreply.github.com>
2026-01-27 23:57:36 -0600

    Day6

---
+++ SQL50/Advanced Select and Joins/1204. Last Person to Fit in the Bus.sql
@@ -0,0 +1,10 @@
+WITH cumulative_weight AS (
+    SELECT person_name, weight,
+           SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM Queue
+)
+SELECT person_name
+FROM cumulative_weight
+WHERE total_weight <= 1000
+ORDER BY total_weight DESC
+LIMIT 1;
