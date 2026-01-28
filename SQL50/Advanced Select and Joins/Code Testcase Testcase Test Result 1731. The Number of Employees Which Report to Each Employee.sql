1e94d273de2dd0dc533d9f7f431a68e5215406b5
Yash Chetan Doshi <132264642+candelatesla@users.noreply.github.com>
2026-01-27 22:45:01 -0600

    Day6

---
+++ SQL50/Advanced Select and Joins/Code Testcase Testcase Test Result 1731. The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,6 @@
+SELECT m.employee_id, m.name,COUNT(e.employee_id) AS reports_count, ROUND(AVG(e.age), 0) AS average_age
+FROM Employees m
+JOIN Employees e
+  ON e.reports_to = m.employee_id
+GROUP BY m.employee_id, m.name
+ORDER BY m.employee_id;
