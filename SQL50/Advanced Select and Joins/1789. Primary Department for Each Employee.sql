990be5047dfd6e74bc408a0568da3c103320fe73
Yash Chetan Doshi <132264642+candelatesla@users.noreply.github.com>
2026-01-27 23:04:16 -0600

    Day5

---
+++ SQL50/Advanced Select and Joins/1789. Primary Department for Each Employee.sql
@@ -0,0 +1,12 @@
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+GROUP BY employee_id
+UNION
+SELECT employee_id, department_id
+FROM Employee
+WHERE employee_id NOT IN (
+    SELECT employee_id
+    FROM Employee
+    WHERE primary_flag = 'Y'
+);
